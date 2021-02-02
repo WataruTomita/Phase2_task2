8b34d942fce4ee82b4a049fdadc07ee0e7b70ee3
WataruTomita <tomi.symphonia@gmail.com>
2021-02-02 10:26:27 +0900

    editer not redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,8 @@ class ApplicationController < ActionController::Base
 
   protected
   def after_sign_in_path_for(resource)
-    books_path
+    # books_path
+    user_path(resource)
   end
 
   def after_sign_out_path_for(resource)
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -38,7 +38,7 @@ class BooksController < ApplicationController
     end
   end
 
-  def delete
+  def destroy
     @book = Book.find(params[:id])
     @book.destroy
     redirect_to books_path
@@ -47,7 +47,7 @@ class BooksController < ApplicationController
   private
 
   def book_params
-    params.require(:book).permit(:title)
+    params.require(:book).permit(:title, :body)
   end
 
 end
