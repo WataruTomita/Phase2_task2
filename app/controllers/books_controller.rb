68b10d6934e3b550ef373a57d1004d2b902f8b18
WataruTomita <tomi.symphonia@gmail.com>
2021-02-02 09:53:32 +0900

    book.rb belongs_to:user

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,7 +2,7 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
-    @user = User.find(params[:id])
+    @user = User.find_by(params[:id])
   end
 
   def index
