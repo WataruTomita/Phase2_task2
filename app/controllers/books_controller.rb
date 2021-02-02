9c8087308a756f2dc1c443011db719352ebfacd0
WataruTomita <tomi.symphonia@gmail.com>
2021-02-02 09:58:49 +0900

    bye bug

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,7 +2,8 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
-    @user = User.find_by(params[:id])
+    # @user = User.find(params[:id])
+    @book_new = Book.new
   end
 
   def index
